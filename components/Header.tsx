c911f1c4c3fed1de35620da0b5e1b4d56d23fbfd
Azamat Meirkhan <azamat.meirkhan@kcell.kz>
2023-05-14 18:17:04 +0600

    Create Protfolio Project

---
+++ components/Header.tsx
@@ -0,0 +1,8 @@
+import React, { type FC } from 'react'
+
+export const Header: FC = () => {
+  return (
+        <header>
+        </header>
+  )
+}
---
+++ pages/page1.tsx
@@ -0,0 +1,5 @@
+export default function Page1() {
+    return (
+        <div>I am Page1</div>
+    )
+}
